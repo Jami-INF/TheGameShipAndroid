38178d65e516fdb6090d8dbb9411b39a9cc76b1c
jainfante <jami.infante@etu.uca.fr>
2022-03-09 15:29:43 +0100

    Persistance Seekbar dificulty fonctionnel, sauvegarde la position de la dificulté sauvegardé a la sortie, et bouton reset fonctionnel

---
+++ Code/app/src/main/java/com/iut/thegameship/Manager.java
@@ -0,0 +1,5 @@
+package com.iut.thegameship;
+
+public class Manager {
+
+}
--- Code/app/src/main/java/com/iut/thegameship/UI/Activities/SettingsActivity.java
+++ Code/app/src/main/java/com/iut/thegameship/UI/Activities/SettingsActivity.java
@@ -7,12 +7,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 
 import com.iut.thegameship.R;
+import com.iut.thegameship.data.FileLoader;
+import com.iut.thegameship.data.FileSaver;
+import com.iut.thegameship.data.ILoad;
+import com.iut.thegameship.data.ISave;
+import com.iut.thegameship.data.Stub;
+import com.iut.thegameship.model.score.Score;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class SettingsActivity extends MainActivity {
+
+    int positionSeekBarDificulty;
+    public static final String PATHToSeekBarDificulty = "SeekBarDificulty";
+    private ISave save = new FileSaver();
+    private ILoad loader;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -22,16 +37,62 @@ public class SettingsActivity extends MainActivity {
         setContentView(R.layout.settings);
         Log.d("Create","onCreateScores()");
 
-        final Button button = findViewById(R.id.backButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onBackPressed();
             }
         });
+        loader = new FileLoader();
+        try {
+            positionSeekBarDificulty = (int) loader.load(openFileInput(PATHToSeekBarDificulty));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBarDificulty);
+        seekBar.setProgress(positionSeekBarDificulty);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                System.out.println("Seeekbar position is : " +i);
+                positionSeekBarDificulty = i;
+                System.out.println(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        final Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                seekBar.setProgress(1);
+            }
+        });
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            save.save(openFileOutput(PATHToSeekBarDificulty, MODE_PRIVATE), positionSeekBarDificulty);
+            System.out.println("save ok");
+            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                scores.forEach((n) -> System.out.println(n.getPseudo()));
+            }*/
+
+        } catch (FileNotFoundException e) {
+
+        }
     }
 
-    public static Intent newIntent(Context context){
-        Intent intent = new Intent(context, SettingsActivity.class);
-        return intent;
-    }
 }
