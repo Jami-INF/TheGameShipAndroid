3aa32fbad043776b38c588697ec94437626b024e
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2022-03-12 16:22:32 +0100

    Ajout activité endGame responsable de sauvegarder le score

---
+++ Code/app/src/main/java/com/iut/thegameship/UI/Activities/EndGameActivity.java
@@ -0,0 +1,51 @@
+package com.iut.thegameship.UI.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.iut.thegameship.R;
+
+public class EndGameActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.end_game);
+        Log.d("Create","onCreateEndGameActivity()");
+
+        final Button backButton = findViewById(R.id.backToMenu);
+        backButton.setOnClickListener(e -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+        final Button playAgainButton = findViewById(R.id.playAgain);
+        playAgainButton.setOnClickListener(e -> {
+            String nickname = "Jean michel tmp";// a mettre le pseudo précedent
+            Intent intent = GameActivity.newIntent(this, nickname);
+            startActivity(intent);
+        });
+
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    public static Intent newIntent(Context context, String nickname, double scorePlayer){
+        Intent intent = new Intent(context, EndGameActivity.class);
+        intent.putExtra("nickname",nickname);
+        intent.putExtra("scorePlayer", scorePlayer);
+        return intent;
+
+    }
+}
