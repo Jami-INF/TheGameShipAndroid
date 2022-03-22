c2625577c78eca323c3848de99bfa8b0fe2fdffe
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2022-03-22 22:22:56 +0100

    Ajout scroll bar dans menu score si y'en a trop

--- Code/app/src/main/java/com/iut/thegameship/util/adapter/ViewHolderScore.java
+++ Code/app/src/main/java/com/iut/thegameship/util/adapter/ViewHolderScore.java
@@ -1,5 +1,6 @@
 package com.iut.thegameship.util.adapter;
 
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ public class ViewHolderScore extends RecyclerView.ViewHolder {
     public ViewHolderScore(@NonNull View itemView){
         super(itemView);
         textViewScore = itemView.findViewById(R.id.scoreText);
+        //textViewScore.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
     }
 
     public TextView getTextViewScore() {
